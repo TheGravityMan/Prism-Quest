db528b4d55c26b8e5748613ab5bde4305b3868d2
pshank <31517807+pshank@users.noreply.github.com>
2022-01-07 14:13:46 -0600

    updating provision process

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except:
 
 setup(
     name="PrismQuest",
-    version="1.0.2",
+    version="1.0.3",
     author="The Gravity Man",
     author_email="patrickjshanks@gmail.com.com",
     description="""Prism Quest is a text-based game.""",
